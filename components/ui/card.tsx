7c66a261250ca8afabef4c06ed4ae41ee5079e30
Emad Alareefi <emad.alareefi@gmail.com>
2026-06-08 11:01:54 +0300

    a

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -22,6 +22,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
+    dir="rtl"
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
